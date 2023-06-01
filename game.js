b3a9e3f098e9ccdeca73de99d5f383d4f49a513a
advait0603 <advaitlinux11@gmail.com>
2023-06-01 21:01:36 +0530

    Some more (wrong) changes to figure out a better approach to the right solution.

--- game.js
+++ game.js
@@ -1,11 +1,3 @@
-let choices = ['rock', 'paper', 'scissor']
-let compScore = 0, playerScore = 0, ties = 0;
-let imgRock = document.querySelector('#imgRock');
-let imgPaper = document.querySelector('#imgPaper');
-let imgScissor = document.querySelector('#imgScissor');
-let scoreboard = document.querySelector('#score');
-let roundResult = document.querySelector('#roundResult')
-
 scoreboard.setAttribute('style', 'font-size: 35px; margin-top: 3%');
 scoreboard.innerHTML = 'Player (you) : '+ playerScore + ' | Computer : '+ compScore + '| Ties : '+ ties;
 
@@ -35,17 +27,15 @@ imgPaper.addEventListener('click', ()=>{
     //alert(compChoice + ' ' + compChoiceIndex);
     if (compChoiceIndex == 0){
 
-        scoreboard.innerHTML = 'Player (you) : '+ (playerScore+1) + ' | Computer : '+ compScore + '| Ties : '+ ties;
+        
         roundResult.innerHTML = 'Computer chose ' + compChoice + ', you chose paper. You\'ve won!'; 
     }
     else if (compChoiceIndex == 1){
-
-        scoreboard.innerHTML = 'Player (you) : '+ playerScore + ' | Computer : '+ compScore + '| Ties : '+ (ties+1);
+         
         roundResult.innerHTML = 'Computer chose ' + compChoice + ', you chose paper. Tied!';  
     }
     else if (compChoiceIndex == 2){
-
-        scoreboard.innerHTML = 'Player (you) : '+ playerScore + ' | Computer : '+ (compScore+1) + '| Ties : '+ ties;
+        
         roundResult.innerHTML = 'Computer chose ' + compChoice + ', you chose paper. You\'ve lost this round!'; 
     }
     
@@ -57,15 +47,15 @@ imgScissor.addEventListener('click', ()=>{
     //alert(compChoice + ' ' + compChoiceIndex);
     if (compChoiceIndex == 0){
 
-        scoreboard.innerHTML = 'Player (you) : '+ playerScore + ' | Computer : '+ (compScore+1) + '| Ties : '+ ties;
+        
         roundResult.innerHTML = 'Computer chose ' + compChoice + ', you chose scissor. You\'ve lost this round!'; 
     }
     else if (compChoiceIndex == 1){
-        scoreboard.innerHTML = 'Player (you) : '+ (playerScore+1) + ' | Computer : '+ compScore + '| Ties : '+ ties;
+        
         roundResult.innerHTML = 'Computer chose ' + compChoice + ', you chose scissor. You\'ve won this round!';  
     }
     else if (compChoiceIndex == 2){
-        scoreboard.innerHTML = 'Player (you) : '+ playerScore + ' | Computer : '+ compScore + '| Ties : '+ (ties+1);
+        
         roundResult.innerHTML = 'Computer chose ' + compChoice + ', you chose scissor. Tied!'; 
     }
     
